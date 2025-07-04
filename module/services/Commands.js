05da9426f7008fcd601c998feca344818e2166ed
errorxxzzz <viniciusbezerragjgj@gmail.com>
2025-07-04 14:11:20 -0300

    Update Commands.js

--- module/services/Commands.js
+++ module/services/Commands.js
@@ -1,4 +1,5 @@
 const EventEmitter = require("events");
+
 class Commands extends EventEmitter {
   constructor({ wsId, ws, send, disconnect, emitter }) {
     super();
@@ -7,20 +8,13 @@ class Commands extends EventEmitter {
     this.send = send;
     this.disconnect = disconnect;
     this.emitter = emitter;
-
-    this.state = {
-      myId: undefined,
-      roomId: undefined,
-      users: [],
-    };
-
+    this.state = { myId: undefined, roomId: undefined, users: [] };
     this.handlers = new Map();
 
     this.register("42[49", (data) => {
       const room = data[1] + data[2];
       const paddedRoom = room.padStart(6, "0");
       this.state.roomId = paddedRoom;
-
       this.emitter?.emit("ROOM_LINK", paddedRoom);
       console.log("ROOM: ", paddedRoom);
     });
@@ -31,38 +25,27 @@ class Commands extends EventEmitter {
         username: user.userName.toLowerCase(),
         avatar: user.avatar,
       }));
-      // console.log("USUARIOS: ", this.state.users)
-      //       this.emitter?.emit("usersUpdated", this.state.users);
     });
 
     this.register("42[4", (data) => {
       const username = data[3].toLowerCase();
       const avatar = data[data.length - 1];
       const index = this.state.users.findIndex((u) => u.username === username);
-      if (index !== -1) {
-        this.state.users[index].avatar = avatar;
-      } else {
-        this.state.users.push({ username, avatar });
-      }
-      // console.log("USUARIOS: ", this.state.users)
-      // this.emitter?.emit("avatarUpdated", { username, avatar });
+      if (index !== -1) this.state.users[index].avatar = avatar;
+      else this.state.users.push({ username, avatar });
     });
 
     this.register("42[20", (data) => {
       if (data[1] == this.state.myId) return;
       this.emitter?.emit("bonk_chat_message", {
         message: data,
-        sendMessage: (text) => {
-          this.send(`42[10,{"message":"${text}"}]`);
-        },
+        sendMessage: (text) => this.send(`42[10,{"message":"${text}"}]`),
       });
     });
   }
 
   register(command, handler) {
-    if (this.handlers.has(command)) {
-      throw new Error(`[HandlerCommand] Comando "${command}" já existe.`);
-    }
+    if (this.handlers.has(command)) throw new Error(`[HandlerCommand] Comando "${command}" já existe.`);
     this.handlers.set(command, handler);
   }
 
